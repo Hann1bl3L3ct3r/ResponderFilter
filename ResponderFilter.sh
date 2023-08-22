2429fb3790b27ac5690388b74526f3c5747d1815
DredgenWhite <141247795+DredgenWhite@users.noreply.github.com>
2023-08-22 15:26:41 -0400

    Update ResponderFilter.sh

--- ResponderFilter.sh
+++ ResponderFilter.sh
@@ -1,5 +1,5 @@
-for user in `strings "/usr/share/responder/logs/Responder-Session.log" | grep "NTLMv2 Hash" | cut -d ":" -f 4-6 | sort -u -f | awk '{$1=$1};1'`
+for user in `strings "/usr/share/responder/logs/Responder-Session.log" | grep -e "NTLMv2 Hash" -e "NTLMv2-SSP Hash" | cut -d ":" -f 4-6 | sort -u -f | awk '{$1=$1};1'`
 do
 echo "[*] search for: $user";
-strings "/usr/share/responder/logs/Responder-Session.log" | grep "NTLMv2 Hash" | grep -i $user | cut -d ":" -f 4-10 |  head -n 1 | awk '{$1=$1};1' >> ntlm-hashes.txt
+strings "/usr/share/responder/logs/Responder-Session.log" | grep -e "NTLMv2 Hash" -e "NTLMv2-SSP Hash"  | grep -i $user | cut -d ":" -f 4-10 |  head -n 1 | awk '{$1=$1};1' >> ntlm-hashes.txt
 done
