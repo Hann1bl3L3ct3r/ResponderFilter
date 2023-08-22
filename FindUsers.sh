9d1a4faa69f59816fe79d2d1a657e3bbbd5f5a6a
DredgenWhite <141247795+DredgenWhite@users.noreply.github.com>
2023-08-22 16:05:50 -0400

    Update FindUsers.sh
    Updating grep to look for two strings instead of one.

--- FindUsers.sh
+++ FindUsers.sh
@@ -1,1 +1,1 @@
-strings "/usr/share/responder/logs/Responder-Session.log" | grep "NTLMv2 Hash" | cut -d ":" -f 4-6 | sort -u -f | awk '{$1=$1};1'
+strings "/usr/share/responder/logs/Responder-Session.log" | grep -e "NTLMv2 Hash" -e "NTLMv2-SSP Hash" | cut -d ":" -f 4-6 | sort -u -f | awk '{$1=$1};1'
